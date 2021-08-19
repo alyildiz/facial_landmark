a39f000ce47604fd989a8c5c150d61e49280eced
Baris ALYILDIZ <baris@bariss-macbook-air.home>
2021-08-19 17:45:12 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Real time facial landmark on CPU',
+    author='ALYILDIZ Baris',
+    license='MIT',
+)
